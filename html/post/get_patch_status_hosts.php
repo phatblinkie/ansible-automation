73583263718e311220c35eb1af188107576a6ed6
phatblinkie <phatblinkie@hotmail.com>
2024-12-26 23:05:04 -0600

    changes to hosts table

--- html/post/get_patch_status_hosts.php
+++ html/post/get_patch_status_hosts.php
@@ -16,7 +16,7 @@ $project_id = $_GET['project_id'];
 
 // Fetch hosts and number of available updates from patching_status table
 $query = "
-    SELECT hostname, found_update_count AS available_updates
+    SELECT hostname, timestamp, found_update_count AS available_updates
     FROM patching_status
     WHERE project_id = ?
     AND timestamp = (
@@ -33,6 +33,7 @@ $result = $stmt->get_result();
 
 $hosts = [];
 while ($row = $result->fetch_assoc()) {
+    $row['formatted_timestamp'] = formatTimestamp($row['timestamp']);
     $hosts[] = $row;
 }
 
@@ -41,4 +42,24 @@ $conn->close();
 
 header('Content-Type: application/json');
 echo json_encode($hosts);
+
+function formatTimestamp($timestamp) {
+    $datetime1 = new DateTime($timestamp);
+    $datetime2 = new DateTime();
+    $interval = $datetime1->diff($datetime2);
+
+    if ($interval->y > 0) {
+        return $interval->y . ' year' . ($interval->y > 1 ? 's' : '') . ' ago';
+    } elseif ($interval->m > 0) {
+        return $interval->m . ' month' . ($interval->m > 1 ? 's' : '') . ' ago';
+    } elseif ($interval->d > 0) {
+        return $interval->d . ' day' . ($interval->d > 1 ? 's' : '') . ' ago';
+    } elseif ($interval->h > 0) {
+        return $interval->h . ' hour' . ($interval->h > 1 ? 's' : '') . ' ago';
+    } elseif ($interval->i > 0) {
+        return $interval->i . ' minute' . ($interval->i > 1 ? 's' : '') . ' ago';
+    } else {
+        return 'just now';
+    }
+}
 ?>
